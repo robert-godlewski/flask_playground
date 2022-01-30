8d74577ed4914488b38ec4e8742e705a07e23174
Robert Godlewski <robtechg5@gmail.com>
2022-01-30 15:45:02 -0800

    Finalized the UI and consolidated all on index.html.

--- server.py
+++ server.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world(): return render_template('index.html')
+def hello_world(): return render_template('index.html', times=0, color='black')
 
 @app.route('/play')
-def play_default(): return render_template('play.html', times=3, color='blue')
+def play_default(): return render_template('index.html', times=3, color='turquoise')
 
 @app.route('/play/<x>')
 def play_num(x): 
     max = int(x)
-    return render_template('play.html', times=max, color='blue')
+    return render_template('index.html', times=max, color='turquoise')
 
 @app.route('/play/<x>/<color>')
 def play(x, color):
     max=int(x)
-    return render_template('play.html', times=max, color=color)
+    return render_template('index.html', times=max, color=color)
 
 
 if __name__=="__main__": app.run(debug=True)
